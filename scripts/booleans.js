f1965a87ab8bb2aba964868adbc5e00d4ed9af54
unknown <GrahamE@ywcanwil.org>
2023-05-24 20:03:34 -0500

    Practice with booleans

---
+++ scripts/booleans.js
@@ -0,0 +1,103 @@
+// * BOOLEAN REVIEW
+// All of these exerc
+
+
+/**
+ * Given two numbers called num1 and num2, check if num1 and num2 are equal.
+ * 
+ * Return true if num1 is equal to num2 or false otherwise.
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns {Boolean}
+ */
+function oneEqualsTwo(num1, num2) {
+    // TODO
+}
+
+
+/**
+ * Given two numbers called num1 and num2, check if num1 is less than num2. 
+ * 
+ * Return true if num1 is less than num2.
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns {Boolean}
+ */
+function oneLessThanTwo(num1, num2) {
+    // TODO
+}
+
+
+/**
+ * Given a number called num, determine if the number is even. Any number that
+ * is divisible by 2 is even.
+ * 
+ * Return true if it is even or false otherwise.
+ * 
+ * @param {Number} num 
+ */
+function isEven(num) {
+    // TODO
+}
+
+
+/**
+ * Given a number called num, determine if that number is odd. Any number that 
+ * is odd is NOT divisible by 2.
+ * 
+ * Return true if the number is odd or false otherwise.
+ * 
+ * @param {Number} num 
+ */
+function isOdd(num) {
+    // TODO
+}
+
+
+/**
+ * Given three numbers, num1, num2, and num3, check that num1 is greater than
+ * num2 and that num1 is less than num3.
+ * 
+ * Return a boolean.
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @param {Number} num3 
+ * @returns {Boolean}
+ */
+function complexBoolean1(num1, num2, num3) {
+    // TODO
+}
+
+
+/**
+ * Given four numbers, num1, num2, num3, and num4, check if all numbers are the
+ * same.
+ * 
+ * Return a boolean.
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @param {Number} num3 
+ * @param {Number} num4 
+ * @returns {Boolean}
+ */
+function complexBoolean2(num1, num2, num3, num4) {
+    // TODO
+}
+
+
+/**
+ * Given numbers num1 and num2, check if num1 * 2 is greater than 15 AND if num2
+ * is divisible by num1.
+ * 
+ * Return a boolean.
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ */
+function complexBoolean3(num1, num2) {
+    // TODO
+}
